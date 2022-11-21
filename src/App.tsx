6ed5a6fa450d1e556226183194b15aa63f22314f
Andrés Rodart <arodartel1601@alumno.ipn.mx>
2022-11-21 03:43:37 -0600

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import ArrowForwardCircle from 'react-ionicons/lib/ArrowForwardCircle';
 import './App.css';
+import { Button } from './components/Button';
+import { Pill } from './components/Pill';
 
 function App() {
 	return (
 		<div className="App">
-			
+			<Pill content='Meta'/>
+			<Button color='action'>
+				<p>
+					Solve
+				</p>
+				<ArrowForwardCircle
+					color={'#ffffff'} 
+					style={{
+						display: 'flex',
+						height: '20px',
+						width: '20px',
+					}}
+				/>
+			</Button>
 		</div>
 	);
 }
